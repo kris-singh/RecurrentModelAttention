52032afc0b89ea68679ab627227da2599eaf9858
kris-singh <krishnakant.singh@suiit.ac.in>
2019-10-23 08:40:28 +0900

    Feat: Add clean run

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -1,6 +1,9 @@
 import logging
 import os
 import sys
+import shutil
+import time
+
 
 
 def setup_logger(name, save_dir, distributed_rank):
@@ -32,25 +35,35 @@ def early_stopping(losses, paitence):
             wait = 0
             return wait, 0
 
-def setup_exp(exp_name, save_dir):
+def setup_exp(save_dir, exp_name, clean_run):
     if not os.path.exists(os.path.join(save_dir, exp_name)):
-        os.mkdir(os.path.join(save_dir, exp_name))
+        os.makedirs(os.path.join(save_dir, exp_name))
     base_name = os.path.join(save_dir, exp_name)
     # runs
     tb_root = os.path.join(save_dir, 'runs')
+    tb_dir = os.path.join(tb_root, exp_name)
+    log_dir = os.path.join(base_name, 'logs')
+    chkpt_dir = os.path.join(base_name, 'chkpt')
+
+    if clean_run:
+        if os.path.exists(tb_dir):
+            shutil.rmtree(tb_dir)
+            time.sleep(60)
+        if os.path.exists(log_dir):
+            shutil.rmtree(log_dir)
+        if os.path.exists(chkpt_dir):
+            shutil.rmtree(chkpt_dir)
+
+
     if not os.path.exists(os.path.join(tb_root, exp_name)):
-        tb_dir = os.mkdir(os.path.join(tb_root, exp_name))
-    else:
-        tb_dir = os.path.join(tb_root, exp_name)
+        os.makedirs(os.path.join(tb_root, exp_name))
+    tb_dir = os.path.join(tb_root, exp_name)
     # logs
     if not os.path.exists(os.path.join(base_name, 'logs')):
-        log_dir = os.mkdir(os.path.join(base_name, 'logs'))
-    else:
-        log_dir = os.path.join(base_name, 'logs')
+        os.makedirs(os.path.join(base_name, 'logs'))
+    log_dir = os.path.join(base_name, 'logs')
     # chkpoint
     if not os.path.exists(os.path.join(base_name, 'chkpt')):
-        chkpt_dir = os.mkdir(os.path.join(base_name, 'chkpt'))
-    else:
-        chkpt_dir = os.path.join(base_name, 'chkpt')
-
+        os.makedirs(os.path.join(base_name, 'chkpt'))
+    chkpt_dir = os.path.join(base_name, 'chkpt')
     return chkpt_dir, log_dir, tb_dir
